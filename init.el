43fd5fd9893bdbe903af6cffb5e34017c74a67a0
Bradley Wright <brad@intranation.com>
2013-08-25 23:06:38 +0100

    Add ansi-term configuration

--- init.el
+++ init.el
@@ -208,6 +208,19 @@
 ;; point
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 
+;;; ansi-term configuration
+;; force ansi-term to be utf-8 after it launches
+(defadvice ansi-term (after advise-ansi-term-coding-system activate)
+  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+
+;; close the terminal buffer automatically on exit
+(defadvice term-sentinel (around my-advice-term-sentinel (proc msg) activate)
+  (if (memq (process-status proc) '(signal exit))
+      (let ((buffer (process-buffer proc)))
+        ad-do-it
+        (kill-buffer buffer))
+    ad-do-it))
+
 (require 'setup-elpa)
 
 ;; Load custom file last
