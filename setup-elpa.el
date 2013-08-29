f6a3d816c06cb038675cb6a99f683f30f99327ff
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-08-29 11:46:39 +0100

    Add markdown-mode configuration

--- setup-elpa.el
+++ setup-elpa.el
@@ -132,4 +132,25 @@
 (require 'setup-ruby)
 (require 'setup-evil)
 
+;;; markdown-mode - for editing markdown
+(require-package 'markdown-mode)
+(after-load 'markdown-mode
+  (setq markdown-imenu-generic-expression
+        '(("title"  "^\\(.*\\)[\n]=+$" 1)
+          ("h2-"    "^\\(.*\\)[\n]-+$" 1)
+          ("h1"   "^# \\(.*\\)$" 1)
+          ("h2"   "^## \\(.*\\)$" 1)
+          ("h3"   "^### \\(.*\\)$" 1)
+          ("h4"   "^#### \\(.*\\)$" 1)
+          ("h5"   "^##### \\(.*\\)$" 1)
+          ("h6"   "^###### \\(.*\\)$" 1)
+          ("fn"   "^\\[\\^\\(.*\\)\\]" 1)))
+
+  (add-hook 'markdown-mode-hook
+            (lambda ()
+              (setq imenu-generic-expression markdown-imenu-generic-expression))))
+
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+
 (provide 'setup-elpa)
