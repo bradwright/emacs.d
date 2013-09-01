012842369b887b90268b17385bf48a729a8d2f00
Bradley Wright <brad@intranation.com>
2013-09-01 09:49:52 +0100

    osx: Handle copy/paste in terminal display

--- init.el
+++ init.el
@@ -106,7 +106,31 @@
     (setq ns-command-modifier 'super)
 
     ;; set fn to hyper
-    (setq ns-function-modifier 'hyper)))
+    (setq ns-function-modifier 'hyper))
+
+  (unless (display-graphic-p)
+    ;; Configuration to make Emacs run semi-normally in an OS X
+    ;; terminal
+
+    ;; XXX: strongly recommended to run in iTerm2, as it's more
+    ;; configurable than Terminal.app.
+
+    ;; Make sure cut/paste works properly. Gotten from:
+    ;; http://mindlev.wordpress.com/2011/06/13/emacs-in-a-terminal-on-osx/#comment-20
+    (defun bw/copy-from-osx ()
+      "Copies the current clipboard content using the `pbcopy` command"
+      (shell-command-to-string "pbpaste"))
+
+    (defun bw/paste-to-osx (text &optional push)
+      "Copies the top of the kill ring stack to the OSX clipboard"
+      (let ((process-connection-type nil))
+        (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+          (process-send-string proc text)
+          (process-send-eof proc))))
+
+    ;; Override defaults to use the mac copy and paste
+    (setq interprogram-cut-function 'bw/paste-to-osx)
+    (setq interprogram-paste-function 'bw/copy-from-osx)))
 
 ;;; Remap execute-extended-command
 ;; This is Yegge's advice
