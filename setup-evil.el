e6e6cefc9312a4a1e21f37c01c780c7f5734a60e
Bradley Wright <brad@intranation.com>
2013-09-06 10:29:41 +0100

    evil: don't enable linum mode by default
    Also tighten up the logic:
    
    - Disable linum mode when entering Emacs
    - Flip to relative only on normal entry (even if not shown)
    - Flip to normal formate on insert mode (even if not shown)

--- setup-evil.el
+++ setup-evil.el
@@ -94,17 +94,14 @@
         ;; show >> on line where cursor is
         linum-relative-current-symbol ">>")
 
-  ;; turn off linum mode when entering Emacs
-  (add-hook 'evil-emacs-state-entry-hook 'bw/disable-linum-mode)
-  ;; enable it again when leaving Emacs state
-  (add-hook 'evil-normal-state-entry-hook 'bw/enable-linum-mode)
-  (add-hook 'evil-insert-state-entry-hook 'bw/enable-linum-mode)
-
-  ;; in Normal mode, show relative numbering
+  ;; in Normal mode, use relative numbering
   (add-hook 'evil-normal-state-entry-hook 'bw/linum-relative-formatting)
-  ;; when leaving normal state, restore normal linum behaviour
-  (add-hook 'evil-normal-state-exit-hook 'bw/linum-normal-formatting)
+  ;; in Insert mode, use normal line numbering
   (add-hook 'evil-insert-state-entry-hook 'bw/linum-normal-formatting)
+  ;; turn off linum mode automatically when entering Emacs mode
+  (add-hook 'evil-emacs-state-entry-hook 'bw/disable-linum-mode)
+  ;; turn off linum mode when entering Emacs
+  (add-hook 'evil-emacs-state-entry-hook 'bw/linum-normal-formatting)
 
   ;; copy linum face so it doesn't look weird
   (set-face-attribute 'linum-relative-current-face nil :foreground (face-attribute 'font-lock-keyword-face :foreground) :background nil :inherit 'linum :bold t))
