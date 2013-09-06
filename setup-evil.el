d2be1ae8e518c5dd1a1190013528eec57b804ae2
Bradley Wright <brad@intranation.com>
2013-09-06 20:12:35 +0100

    evil: evil-leader is mostly autoloaded
    Also remove ',' as <leader> key

--- setup-evil.el
+++ setup-evil.el
@@ -1,37 +1,32 @@
 ;;; evil-leader - ViM leader key for evil-mode
 ;;; https://github.com/cofi/evil-leader
 (require-package 'evil-leader)
-(after-load 'evil-leader
-  (global-evil-leader-mode 1)
-  ;; keyboard shortcuts
-  (evil-leader/set-leader ",")
-  (evil-leader/set-key
-    "a" 'ag-project
-    "A" 'ag
-    "b" 'ido-switch-buffer
-    "c" 'mc/mark-next-like-this
-    "C" 'mc/mark-all-like-this
-    "e" 'er/expand-region
-    "E" 'mc/edit-lines
-    "f" 'ido-find-file
-    "g" 'magit-status
-    "i" 'idomenu
-    "j" 'ace-jump-mode
-    "k" 'kill-buffer
-    "K" 'kill-this-buffer
-    "l" 'linum-mode
-    "o" 'occur
-    "p" 'magit-find-file-completing-read
-    "r" 'bw/recentf-ido-find-file
-    "s" 'ag-project
-    "t" 'bw/open-term
-    "T" 'eshell
-    "w" 'save-buffer
-    "x" 'smex
-    )
-  )
-(require 'evil-leader)
 
+(global-evil-leader-mode 1)
+;; keyboard shortcuts
+(evil-leader/set-key
+  "a" 'ag-project
+  "A" 'ag
+  "b" 'ido-switch-buffer
+  "c" 'mc/mark-next-like-this
+  "C" 'mc/mark-all-like-this
+  "e" 'er/expand-region
+  "E" 'mc/edit-lines
+  "f" 'ido-find-file
+  "g" 'magit-status
+  "i" 'idomenu
+  "j" 'ace-jump-mode
+  "k" 'kill-buffer
+  "K" 'kill-this-buffer
+  "l" 'linum-mode
+  "o" 'occur
+  "p" 'magit-find-file-completing-read
+  "r" 'bw/recentf-ido-find-file
+  "s" 'ag-project
+  "t" 'bw/open-term
+  "T" 'eshell
+  "w" 'save-buffer
+  "x" 'smex)
 
 ;;; Evil - Emacs vi emulation
 ;;; http://gitorious.org/evil/pages/Home
