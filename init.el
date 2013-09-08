b091909eb3619ceef3359d09c8bbd5f66b187656
Bradley Wright <brad@intranation.com>
2013-09-08 22:05:22 +0100

    Allow tramp to sudo edit over SSH

--- init.el
+++ init.el
@@ -324,6 +324,19 @@ keyboard-quit"
 ;;; python-mode - Python major mode
 (add-hook 'python-mode-hook 'bw/turn-on-subword-mode)
 
+;;; tramp-mode - Edit files remotely
+(after-load 'tramp
+  ;; use SSH by default
+  (setq tramp-default-method "ssh")
+  ;; allow me to SSH to hosts and edit as sudo like:
+  ;;   C-x C-f /sudo:example.com:/etc/something-owned-by-root
+  ;; from: http://www.gnu.org/software/tramp/#Multi_002dhops
+  (add-to-list 'tramp-default-proxies-alist
+               '(nil "\\`root\\'" "/ssh:%h:"))
+  (add-to-list 'tramp-default-proxies-alist
+               '((regexp-quote (system-name)) nil nil)))
+
+
 ;;; ELPA customisation and installation
 (require 'setup-elpa)
 
