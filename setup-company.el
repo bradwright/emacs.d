e9814407837920eabd78df1c2752b0a5aa358ea1
Bradley Wright <brad@intranation.com>
2013-12-27 17:12:53 +0000

    company: Style changes and adjust defaults

--- setup-company.el
+++ setup-company.el
@@ -9,8 +9,15 @@
 
 (after-load 'company
   (global-company-mode t)
+  ;; Make sure emacs does the right thing with completion command
   (global-set-key (kbd "M-/") 'company-complete)
-  (setq evil-complete-next-func 'bw/company-complete-lambda
+  (setq company-minimum-prefix-length 0 ;; autocomplete right after '.'
+        ;; shorter delay
+        company-idle-delay .3
+        ;; remove echo delay
+        company-echo-delay 0
+        ;; make sure evil uses the right completion functions
+        evil-complete-next-func 'bw/company-complete-lambda
         evil-complete-previous-func 'bw/company-complete-lambda))
 
 (require 'company)
@@ -20,15 +27,16 @@
 ;;; https://github.com/nsf/gocode/blob/master/emacs-company/company-go.el
 (require-package 'company-go)
 
-(require 'company-go)
-
 (defun bw/setup-company-go ()
   "Hook for running on company-go"
+  ;; we only want to use company-go - it's so accurate we won't need any other
+  ;; completion engines
   (set (make-local-variable 'company-backends) '(company-go)))
 
 (after-load 'company-go
   (add-hook 'go-mode-hook 'bw/setup-company-go))
 
+(require 'company-go)
 
 
 (provide 'setup-company)
