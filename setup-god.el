9e1e631c0b8401b96c46133f3bce66743e171c85
Bradley Wright <brad@intranation.com>
2014-01-19 22:23:56 +0000

    god: toggle cursor appearance

--- setup-god.el
+++ setup-god.el
@@ -3,6 +3,11 @@
 
 (require-package 'god-mode)
 
+(defun bw/god-mode-change-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'hollow
+                      'box)))
+
 (after-load 'god-mode
   (global-set-key (kbd "<escape>") 'god-local-mode)
   ;; press 'i' to enter normal Emacs mode
@@ -10,6 +15,9 @@
 
   ;; don't enable god-mode in terminals
   (add-to-list 'god-exempt-major-modes 'term-mode)
+
+  (add-hook 'god-mode-enabled-hook 'bw/god-mode-change-cursor)
+  (add-hook 'god-mode-disabled-hook 'bw/god-mode-change-cursor)
 
   ;; enable god-mode globally
   (god-mode-all))
