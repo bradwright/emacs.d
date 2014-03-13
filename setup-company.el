e249e6cf870efa2c2f978504c0c85aa6bbbc0405
Bradley Wright <brad@intranation.com>
2014-03-13 13:54:25 +0000

    Install/load company-go when go-mode is loaded
    Otherwise there's a race condition where go-mode might not be installed.

--- setup-company.el
+++ setup-company.el
@@ -32,28 +32,29 @@
 (require 'company)
 
 
-;;; company-go - company backend for Golang
-;;; https://github.com/nsf/gocode/blob/master/emacs-company/company-go.el
-(require-package 'company-go)
+(after-load 'go-mode
+  ;;; company-go - company backend for Golang
+  ;;; https://github.com/nsf/gocode/blob/master/emacs-company/company-go.el
+  (require-package 'company-go)
 
-(defun bw/setup-company-go ()
-  "Hook for running on company-go"
-  ;; we only want to use company-go - it's so accurate we won't need any other
-  ;; completion engines
-  (set (make-local-variable 'company-backends) '(company-go)))
+  (defun bw/setup-company-go ()
+    "Hook for running on company-go"
+    ;; we only want to use company-go - it's so accurate we won't need any other
+    ;; completion engines
+    (set (make-local-variable 'company-backends) '(company-go)))
 
-(after-load 'company-go
-  (defadvice company-go (around fix-company-go-prefix activate)
-    "Clobber company-go to use company-grab-word instead of the
+  (after-load 'company-go
+    (defadvice company-go (around fix-company-go-prefix activate)
+      "Clobber company-go to use company-grab-word instead of the
 flakey regular expression. This allows us to complete standard
 variables etc. as well as methods and properties."
-    ad-do-it
-    (when (eql (ad-get-arg 0) 'prefix)
-      (setq ad-return-value (company-grab-word))))
+      ad-do-it
+      (when (eql (ad-get-arg 0) 'prefix)
+        (setq ad-return-value (company-grab-word))))
 
-  (add-hook 'go-mode-hook 'bw/setup-company-go))
+    (add-hook 'go-mode-hook 'bw/setup-company-go))
 
-(require 'company-go)
+  (require 'company-go))
 
 
 (provide 'setup-company)
