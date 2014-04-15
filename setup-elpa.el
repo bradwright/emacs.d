cbb9ec6a90cdefb7fd00288a191879bde7fa1f2c
Bradley Wright <brad@intranation.com>
2014-04-15 13:07:35 +0100

    Fix typo in 'gnu' package repo name

--- setup-elpa.el
+++ setup-elpa.el
@@ -7,7 +7,7 @@
 
   ;; Only use 3 specific directories
   (setq package-archives
-        '(("gnu "         . "http://elpa.gnu.org/packages/")
+        '(("gnu"          . "http://elpa.gnu.org/packages/")
           ("marmalade"    . "http://marmalade-repo.org/packages/")
           ("melpa"        . "http://melpa.milkbox.net/packages/")
           ("elpy"         . "http://jorgenschaefer.github.io/packages/")
