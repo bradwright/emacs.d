92776905e32dd06b8edd90d5f98fa9a35ac037d3
Bradley Wright <brad@intranation.com>
2014-04-22 10:51:20 +0100

    god: Don't enable god-mode when pressing <escape>

--- setup-god.el
+++ setup-god.el
@@ -10,9 +10,9 @@
                       t)))
 
 (after-load 'god-mode
-  (global-set-key (kbd "<escape>") 'god-local-mode)
-  ;; press 'i' to enter normal Emacs mode
-  (define-key god-local-mode-map (kbd "i") 'god-local-mode)
+  ;; (global-set-key (kbd "<escape>") 'god-local-mode)
+  ;; ;; press 'i' to enter normal Emacs mode
+  ;; (define-key god-local-mode-map (kbd "i") 'god-local-mode)
 
   ;; don't enable god-mode in terminals
   (add-to-list 'god-exempt-major-modes 'term-mode)
