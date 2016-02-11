ebd8b8d76d2e0b9258fe25175f0cd472172fee64
Bradley Wright <brad@intranation.com>
2016-02-11 20:37:29 +0000

    Add commented out package-initialize call
    This is needed by Emacs 25.1, apparently. Not sure why.

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
 
 ;; This file uses org-babel to load my literate configuration.
 
+;; This is required by Emacs 25.1 for some reason.
+;; (package-initialize)
+
 (require 'ob-tangle)
 
 (org-babel-load-file (expand-file-name "README.org" (file-name-directory
